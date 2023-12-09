696eaccc8fd0865caf58dbbe8b2f42a7e44341f5
Dingsel <dshluka@gmail.com>
2023-12-09 13:57:18 +0100

    updates

---
+++ ui1/main.js
@@ -0,0 +1,24 @@
+import { world } from "@minecraft/server"
+import { ActionFormData } from "@minecraft/server-ui"
+
+const ui = new ActionFormData()
+    .title("Form")
+    .body("")
+    .button("button1")
+    .button("button2")
+    .button("button3");
+
+const customUi = new ActionFormData()
+    .title("Custom Form")
+    .body("")
+    .button("button1")
+    .button("button2")
+    .button("button3");
+
+world.afterEvents.itemUse.subscribe((event) => {
+    const { source, itemStack } = event
+    switch (itemStack.typeId) {
+        case "minecraft:compass": ui.show(source); break;
+        case "minecraft:clock": customUi.show(source); break;
+    }
+})
